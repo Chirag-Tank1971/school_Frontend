f97aaa6f48c6bbc05a02c8a5d97dc8c58526579f
Chirag Tank <pcgamingoffical1971@gmail.com>
2025-09-01 19:33:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <title>School Management System</title>
         <meta name="description" content="Manage school information efficiently" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
       </Head>
 
       <main className="main">
